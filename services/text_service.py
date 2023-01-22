b90a5b575652b8a60f537d5f39438c9593aa9db1
Rene Teigen <rene9917@gmail.com>
2023-01-22 00:27:34 +0000

    Use user parameter for displayname if set in encapsulate send

--- services/text_service.py
+++ services/text_service.py
@@ -101,7 +101,7 @@ class TextService:
                 new_prompt = prompt.encode("ascii", "ignore").decode()
                 prompt_less_author = f"{new_prompt} <|endofstatement|>\n"
 
-                user_displayname = ctx.author.display_name
+                user_displayname = ctx.author.display_name if not user else user.display_name
 
                 new_prompt = (
                     f"\n{user_displayname}: {new_prompt} <|endofstatement|>\n"
