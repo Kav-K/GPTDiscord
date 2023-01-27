40bb101f274a01a108419bf40392f7521464d18b
Kaveen Kumarasinghe <ocfinancesmc@gmail.com>
2023-01-27 00:24:56 -0500

    bump version

--- gpt3discord.py
+++ gpt3discord.py
@@ -30,7 +30,7 @@ from services.environment_service import EnvService
 from models.openai_model import Model
 
 
-__version__ = "8.5"
+__version__ = "8.6"
 
 PID_FILE = Path("bot.pid")
 PROCESS = None
