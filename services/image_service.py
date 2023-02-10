ed432952857d9ea3ba71a7c4493437b0b0251139
Kaveen Kumarasinghe <ocfinancesmc@gmail.com>
2023-02-10 16:44:55 -0500

    fix buttons

--- services/image_service.py
+++ services/image_service.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 import tempfile
 import traceback
 from io import BytesIO
@@ -262,7 +263,7 @@ class SaveView(discord.ui.View):
 
 class VaryButton(discord.ui.Button):
     def __init__(self, number, image_url, cog, converser_cog, custom_api_key):
-        super().__init__(style=discord.ButtonStyle.blurple, label="Vary " + str(number), custom_id="vary_button")
+        super().__init__(style=discord.ButtonStyle.blurple, label="Vary " + str(number), custom_id="vary_button"+str(random.randint(10000000,99999999)))
         self.number = number
         self.image_url = image_url
         self.cog = cog
@@ -318,7 +319,7 @@ class VaryButton(discord.ui.Button):
 
 class SaveButton(discord.ui.Button["SaveView"]):
     def __init__(self, number: int, image_url: str):
-        super().__init__(style=discord.ButtonStyle.gray, label="Save " + str(number), custom_id="save_button")
+        super().__init__(style=discord.ButtonStyle.gray, label="Save " + str(number), custom_id="save_button"+str(random.randint(1000000,9999999)))
         self.number = number
         self.image_url = image_url
 
