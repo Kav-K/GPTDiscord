d6e8f74a65361d004f4c7494fe5daef27d06df65
Kaveen Kumarasinghe <k5kumara@uwaterloo.ca>
2023-02-11 05:17:53 -0500

    Update moderations_service.py
    Signed-off-by: Kaveen Kumarasinghe <k5kumara@uwaterloo.ca>

--- services/moderations_service.py
+++ services/moderations_service.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import random
 import traceback
 from datetime import datetime, timedelta
 from pathlib import Path
@@ -411,7 +412,7 @@ class TimeoutUserButton(discord.ui.Button["ModerationAdminView"]):
         super().__init__(
             style=discord.ButtonStyle.danger,
             label=f"Timeout {hours}h",
-            custom_id="timeout_button",
+            custom_id="timeout_button"+str(random.randint(100000,999999)),
         )
         self.message = message
         self.moderation_message = moderation_message
