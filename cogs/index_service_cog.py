734491431f67a3361b2734a2db1519c72c85327f
system32.eth <23362597+Raecaug@users.noreply.github.com>
2023-04-24 17:59:51 -0400

    Update index_service_cog.py
    Use os.environ to update GITHUB_TOKEN environment variable
    
    Signed-off-by: system32.eth <23362597+Raecaug@users.noreply.github.com>

--- cogs/index_service_cog.py
+++ cogs/index_service_cog.py
@@ -2,6 +2,7 @@ import traceback
 from pathlib import Path
 
 import discord
+import os
 
 from models.embed_statics_model import EmbedStatics
 from services.environment_service import EnvService
@@ -13,6 +14,7 @@ USER_INPUT_API_KEYS = EnvService.get_user_input_api_keys()
 USER_KEY_DB = EnvService.get_api_db()
 PRE_MODERATE = EnvService.get_premoderate()
 GITHUB_TOKEN = EnvService.get_github_token()
+os.environ["GITHUB_TOKEN"] = GITHUB_TOKEN
 
 
 class IndexService(discord.Cog, name="IndexService"):
